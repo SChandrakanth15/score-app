d47063f3dc3fbd8bfae30a7b09b346a193e6f35a
SChandrakanth15 <schandrakanth15@gmail.com>
2024-11-04 13:09:14 +0530

    first commit

---
+++ src/main/java/com/cricket/model/Match.java
@@ -0,0 +1,13 @@
+package com.cricket.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Match {
+    private UUID matchId;
+    private String team1;
+    private String team2;
+    private Date matchDate;
+    private String venue;
+    private String status;
+}
